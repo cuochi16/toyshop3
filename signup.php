2ae332716ea55919197ab12d41dad0d61faa35eb
huynqbhaf200130 <98049297+huynqbhaf200130@users.noreply.github.com>
2022-04-16 00:08:22 +0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -37,7 +37,7 @@
             <div class="row">
                 <div class="col-lg-2">
                     <div class="header__logo">
-                        <a href="index.php">
+                        <a href="./index.html">
                             <img src="img/logo.png" alt="">
                         </a>
                     </div>
@@ -46,15 +46,19 @@
                     <div class="header__nav">
                         <nav class="header__menu mobile-menu">
                             <ul>
-                                <li class="active"><a href="index.php">Home</a></li>
-                                <li><a href="categories.php">Manage <span class="arrow_carrot-down"></span></a>
+                                <li><a href="./index.php">Homepage</a></li>
+                                <li><a href="./categories.php">Categories <span class="arrow_carrot-down"></span></a>
                                     <ul class="dropdown">
-                                        <li><a href="categories.php">Categories</a></li>
-                                        <li><a href="listproduct.php">Product</a></li>
-                                        <li><a href="signup.php">Sign Up</a></li>
-                                        <li><a href="login.php">Login</a></li>
+                                        <li><a href="./categories.php">Categories</a></li>
+                                        <li><a href="./anime-details.php">Anime Details</a></li>
+                                        <li><a href="./anime-watching.php">Anime Watching</a></li>
+                                        <li><a href="./blog-details.php">Blog Details</a></li>
+                                        <li><a href="./signup.php">Sign Up</a></li>
+                                        <li><a href="./login.php">Login</a></li>
                                     </ul>
                                 </li>
+                                <li><a href="./blog.php">Our Blog</a></li>
+                                <li><a href="#">Contacts</a></li>
                             </ul>
                         </nav>
                     </div>
@@ -62,7 +66,7 @@
                 <div class="col-lg-2">
                     <div class="header__right">
                         <a href="#" class="search-switch"><span class="icon_search"></span></a>
-                        <a href="login.php"><span class="icon_profile"></span></a>
+                        <a href="./login.html"><span class="icon_profile"></span></a>
                     </div>
                 </div>
             </div>
@@ -70,6 +74,86 @@
         </div>
     </header>
     <!-- Header End -->
+
+    <!-- Normal Breadcrumb Begin -->
+    <section class="normal-breadcrumb set-bg" data-setbg="img/normal-breadcrumb.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="normal__breadcrumb__text">
+                        <h2>Sign Up</h2>
+                        <p>Welcome to the official Anime blog.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Normal Breadcrumb End -->
+
+    <!-- Signup Section Begin -->
+    <section class="signup spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="login__form">
+                        <h3>Sign Up</h3>
+                        <form action="#" method="post">
+                            <div class="input__item">
+                                <input type="text" placeholder="Email address" name="email">
+                                <span class="icon_mail"></span>
+                            </div>
+                            <div class="input__item">
+                                <input type="text" placeholder="Your Name" name="username">
+                                <span class="icon_profile"></span>
+                            </div>
+                            <div class="input__item">
+                                <input type="password" placeholder="Password" name="password">
+                                <span class="icon_lock"></span>
+                            </div>
+                            <div class="input__item">
+                                <input type="text" placeholder="Role" name="role">
+                                <span class="icon_lock"></span>
+                            </div>
+                            <button type="submit" class="site-btn" name="signup">Sign up</button>
+                        </form>
+                        <?php
+                        include 'connect.php';
+                        if(isset($_POST['signup']))
+                            {
+                            $email = $_POST['email'];
+                            $username = $_POST['username'];
+                            $password = $_POST['password'];
+                            $role = $_POST['role'];
+                            $sql ="INSERT INTO `users`(`email`, `username`, `password`, `role`) VALUES ('$email','$username','$password','$role')";
+                            $signup = mysqli_query($connect,$sql);
+                                if($signup){
+                                    echo " Sign-Up Successfully
+                                        <script>alert('Welcome $username, Login Now!');
+                                        window.open('index.php', '_self');</script>";
+                                            }
+                                else{
+                                    echo "Error!";
+                                    }
+                            }
+                        ?>
+                        <h5>Already have an account? <a href="login.php">Log In!</a></h5>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="login__social__links">
+                        <h3>Login With:</h3>
+                        <ul>
+                            <li><a href="#" class="facebook"><i class="fa fa-facebook"></i> Sign in With Facebook</a>
+                            </li>
+                            <li><a href="#" class="google"><i class="fa fa-google"></i> Sign in With Google</a></li>
+                            <li><a href="#" class="twitter"><i class="fa fa-twitter"></i> Sign in With Twitter</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Signup Section End -->
 
     <!-- Footer Section Begin -->
     <footer class="footer">
