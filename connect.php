f04877c2811336918c7852aff114e2490343b6a3
huynqbhaf200130 <98049297+huynqbhaf200130@users.noreply.github.com>
2022-04-16 02:13:09 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
     $severname = "3.132.234.157";
     $username = "huy16";
     $password = "huy16";
-    $database = "huy16/toyshop";
+    $database = "toyshop";
     //Khai báo biến để kết nỗi CSDL
     $connect = new mysqli($severname,$username,$password,$database);
     //$connect = new mysqli_connect ----> Hướng Thủ Tục
