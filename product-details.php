c1ab4a9fe724cc20d8ab0edad96640e42a8df7aa
huynqbhaf200130 <98049297+huynqbhaf200130@users.noreply.github.com>
2022-04-17 18:27:27 +0700

    Update product-details.php

--- product-details.php
+++ product-details.php
@@ -1,6 +1,76 @@
-@extends('user.layouts.master')
-@section('content')
+<!DOCTYPE html>
+<html lang="zxx">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Anime Template">
+    <meta name="keywords" content="Anime, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Anime | Template</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/plyr.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Header Section Begin -->
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2">
+                    <div class="header__logo">
+                        <a href="index.php">
+                            <img src="img/logo.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="header__nav">
+                        <nav class="header__menu mobile-menu">
+                            <ul>
+                                <li class="active"><a href="index.php">Home</a></li>
+                                <li><a href="categories.php">Manage <span class="arrow_carrot-down"></span></a>
+                                    <ul class="dropdown">
+                                        <li><a href="categories.php">Categories</a></li>
+                                        <li><a href="listproduct.php">Product</a></li>
+                                        <li><a href="signup.php">Sign Up</a></li>
+                                        <li><a href="login.php">Login</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="header__right">
+                        <a href="#" class="search-switch"><span class="icon_search"></span></a>
+                        <a href="login.php"><span class="icon_profile"></span></a>
+                    </div>
+                </div>
+            </div>
+            <div id="mobile-menu-wrap"></div>
+        </div>
+    </header>
+    <!-- Header End -->
+    
     <!-- Breadcrumb Begin -->
     <div class="breadcrumb-option">
         <div class="container">
@@ -9,7 +79,6 @@
                     <div class="breadcrumb__links">
                         <a href="./index.html"><i class="fa fa-home"></i> Home</a>
                         <a href="./categories.html">Product</a>
-                        <span>{{$data->product_name}}</span>
                     </div>
                 </div>
             </div>
@@ -22,9 +91,21 @@
         <div class="container">
             <div class="anime__details__content">
                 <div class="row">
+                    <?php
+                        include('connect.php');
+                        $product_id = $_GET["product_id"];
+                        $sql1 = "SELECT * FROM product where product_id = '$product_id'";
+                        $result = mysqli_query($connect, $sql1);
+                        if($row =mysqli_fetch_assoc($result)) 
+                        {
+                            $product_id= $row['product_id'];
+                            $product_name = $row['product_name'];
+                            $product_price = $row['product_price'];
+                            $product_image = $row['product_image'];
+                            $product_des = $row['product_des'];
+                    ?>
                     <div class="col-lg-3">
-                        <div class="anime__details__pic set-bg" data-setbg="../../img/{{$data['product_image']}}">
-                            <img src="../../img/{{$data['product_image']}}" alt="">
+                        <div class="anime__details__pic set-bg" data-setbg="img/<?php echo "$product_image" ?>">
                             <div class="comment"><i class="fa fa-comments"></i> 11</div>
                             <div class="view"><i class="fa fa-eye"></i> 9141</div>
                         </div>
@@ -32,8 +113,8 @@
                     <div class="col-lg-9">
                         <div class="anime__details__text">
                             <div class="anime__details__title">
-                                <h3>{{$data->product_name}}</h3>
-                                <span>{{$data->product_price}}$</span>
+                                <h3><?php echo "$product_name" ?></h3>
+                                <span><?php echo "$product_price" ?>$</span>
                             </div>
                             <div class="anime__details__rating">
                                 <div class="rating">
@@ -45,7 +126,7 @@
                                 </div>
                                 <span>1.029 Votes</span>
                             </div>
-                            <p>{{$data->product_des}}</p>
+                            <p><?php echo "$product_des" ?></p>
                             <div class="anime__details__widget">
                                 <div class="row">
                                     <div class="col-lg-6 col-md-6">
@@ -72,9 +153,112 @@
                                 <a href="#" class="follow-btn"><img src="{{asset('img/cart.png')}}" alt=""> Add to cart</a>
                                 </div>
                             </div>
+                            <div>
+                                                <!-- Button trigger modal -->
+												<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+												Edit
+												</button>
+
+												<!-- Modal -->
+												<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+													<div class="modal-dialog">
+														<div class="modal-content">
+															<div class="modal-header">
+																<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+																<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+															</div>
+															<div class="modal-body">
+                                                            <form action="" method="post" enctype="multipart/form-data">
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Product name</label>
+                                                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Product name" name="product_name">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Product price</label>
+                                                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Price" name="product_price">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Product image</label>
+                                                    <input type="file" class="form-control" id="exampleInputPassword1" placeholder="Image" name="product_image">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Description</label>
+                                                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Description" name="product_des">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1">Category</label>
+                                                    <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Category" name="category_id">
+                                                </div>
+                                                <div class="form-group">
+												<div class="modal-footer">
+													<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+													<button type="submit" class="btn btn-primary" name="edit">Add</button>
+												</div>
+                                            </form>
+                                            <?php
+                                                include 'connect.php';
+                                                if(isset($_POST['edit']))
+                                                {
+                                                    $product_name = $_POST['product_name'];
+                                                    $product_price = $_POST['product_price'];
+                                                    $product_image = $_POST['product_image'];
+                                                    $product_image = $_FILES['product_image']['name'];
+                                                    $product_image_tmp = $_FILES['product_image']['tmp_name'];
+                                                    move_uploaded_file($product_image_tmp,"img/$product_image");
+                                                    $product_des = $_POST['product_des'];
+                                                    $category_id = $_POST['category_id'];
+                                                    $sql = "UPDATE product SET `product_name`='$product_name',`product_price`='$product_price',`product_image`='$product_image',`product_des`='$produce_des',`category_id`='$category_id' WHERE `product_id` ='$product_id'";
+                                                    $add = mysqli_query($connect,$sql);
+                                                    if($add){
+                                                            echo " Edit Successfully
+                                                            <script>alert('Edit successfully');
+                                                            window.open('product-details.php?product_id=$product_id', '_self');</script>";
+                                                    }
+                                                    else{
+                                                            echo "Error!";
+                                                    }
+                                                }
+                                            ?>
+															</div>
+														</div>
+													</div>
+												</div>  
+                                            </div> 
+                                            
+                                       <div>
+                                            <form method="POST">
+                                            <button type="submit" class="btn btn-primary" name="delete" >
+                                                Delete
+                                            </button>
+                                            </form>
+                                            <span><a  class="cartdetail" href="add_cart.php?product_id=<?php echo"$product_id" ?>">
+                                                <img style="width:50%; float:center; padding:5px 0 5px 0" src="images/shopping-cart.png" alt="">
+                                                </a></span>
+                                            <?php
+                                                include ('connect.php');
+                                                if(isset($_POST['delete']))
+                                                {
+                                                    $product_id= $row['product_id'];
+                                                    $product_name = $row['product_name'];
+                                                    $product_price = $row['product_price'];
+                                                    $product_image = $row['product_image'];
+                                                    $product_des = $row['product_des'];
+                                                    $sql = "DELETE FROM product WHERE `product_id`= '$product_id'";
+                                                    $delete = mysqli_query($connect,$sql);
+                                                    if($delete){
+                                                        echo "delete Successfully
+                                                        <script>alert('delete successfully');
+                                                        window.open('listproduct.php', '_self');</script>";
+                                                    }
+                                                    else{
+                                                            echo "Error!";
+                                                    }
+                                                }
+                                            ?>
+                                       </div>       
                         </div>
                     </div>
-                </div>
+                <?php } ?> </div>
                 <div class="row">
                     <div class="col-lg-8 col-md-8">
                         <div class="anime__details__review">
@@ -179,4 +363,62 @@
             </div>
         </section>
         <!-- Anime Section End -->
-@stop
+
+    <!-- Footer Section Begin -->
+    <footer class="footer">
+        <div class="page-up">
+            <a href="#" id="scrollToTopButton"><span class="arrow_carrot-up"></span></a>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="footer__logo">
+                        <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="footer__nav">
+                        <ul>
+                            <li class="active"><a href="./index.html">Homepage</a></li>
+                            <li><a href="./categories.html">Categories</a></li>
+                            <li><a href="./blog.html">Our Blog</a></li>
+                            <li><a href="#">Contacts</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                      Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                      <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+
+                  </div>
+              </div>
+          </div>
+      </footer>
+      <!-- Footer Section End -->
+
+      <!-- Search model Begin -->
+      <div class="search-model">
+        <div class="h-100 d-flex align-items-center justify-content-center">
+            <div class="search-close-switch"><i class="icon_close"></i></div>
+            <form class="search-model-form">
+                <input type="text" id="search-input" placeholder="Search here.....">
+            </form>
+        </div>
+    </div>
+    <!-- Search model end -->
+
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/player.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/mixitup.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
